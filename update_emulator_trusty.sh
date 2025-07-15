d87c18c51e952b4143170f62b23e49395afc16b0
Andrei Homescu <ahomescu@xwf.google.com>
2025-07-15 21:58:20 +0000

    trusty: Rewrite update_emulator_trusty.sh
    The drop_prebuilt.par is running into some permissions issues
    so rewrite update_emulator_trusty.sh to use fetch_artifact,
    using the existing update_emulator_linux.sh script as the base.
    
    Bug: 430121095
    Test: presubmit
    Change-Id: I70312dbff215a1d0ea0f994affb543004e1581ee

--- update_emulator_trusty.sh
+++ update_emulator_trusty.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Find builds here:
-# https://android-build.googleplex.com/builds/branches/aosp-emu-dev/grid?
+# https://android-build.googleplex.com/builds/branches/git_emu-main-next-dev/grid?
 set -e
 
 if [ $# == 1 ]
@@ -11,15 +11,25 @@ else
 	exit 1
 fi
 
-prebuilt_tool="/google/data/ro/projects/android/prebuilt_drop_tool/prebuilt_drop.par"
+manifest_xml="manifest_$build.xml"
+trusty_zip="sdk-repo-trusty_x64-qemu-$build.zip"
+trusty_dir="trusty-x86_64"
 
-common_args=()
-common_args+=(--build_id=$build)
-common_args+=(--target=trusty_tee-trusty_x64)
-common_args+=(--dest_host='https://android-review.googlesource.com')
-common_args+=(--dest_project='platform/prebuilts/android-emulator')
-common_args+=(--dest_git_branch=main)
-common_args+=(--topic="trusty_qemu_build_$build")
+echo Fetching Trusty $build
+/google/data/ro/projects/android/fetch_artifact --bid $build --target emulator_trusty_tee_trusty_x64 "$manifest_xml"
+/google/data/ro/projects/android/fetch_artifact --bid $build --target emulator_trusty_tee_trusty_x64 "$trusty_zip"
 
-$prebuilt_tool "${common_args[@]}" --source_dest_file_pair="manifest_$build.xml:trusty-x86_64/manifest.xml"
-$prebuilt_tool "${common_args[@]}" --source_dest_file_pair="sdk-repo-trusty_x64-qemu-$build.zip:trusty-x86_64/qemu.zip" --transform='unzip' --use_git
+rm -rf "$trusty_dir"
+mkdir "$trusty_dir"
+mv "$manifest_xml" "$trusty_dir/manifest.xml"
+unzip -d "$trusty_dir" "$trusty_zip"
+
+printf "Upgrade Trusty emulator to emu-main-next-dev build $build\n\n" > emulator.commitmsg
+
+# Restore Android.bp since we deleted the whole directory earlier
+git restore -W "$trusty_dir/Android.bp"
+git add "$trusty_dir"
+git commit -s -t emulator.commitmsg
+
+rm -f "emulator.commitmsg"
+rm -f "$trusty_zip"
